53368d2027f6849badf7f416fdbc4b60609bc1fa
datquangtran <45448911+datquangtran@users.noreply.github.com>
2020-12-16 01:58:35 +0700

    src

---
+++ J2.L.P0022/model/Memory.java
@@ -0,0 +1,20 @@
+
+package model;
+
+public class Memory {
+
+    private double m;
+
+    public Memory(double m) {
+        this.m = m;
+    }
+
+    public double getM() {
+        return m;
+    }
+
+    public void setM(double m) {
+        this.m = m;
+    }
+
+}
